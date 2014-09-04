4a9a7d1eeeb71b84ae1f9f06479bf49a4b16a4dd
Walter Schweitzer <wschweitzer00@gmail.com>
2014-09-04 10:32:13 -0400

    added stars controller

---
+++ src/Entertainment/Bundle/GuideToTheStarsBundle/Controller/StarController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Entertainment\Bundle\GuideToTheStarsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class StarController extends Controller
+{
+    
+     /**
+     * @Route("/stars/index")
+     */
+    public function indexAction()
+    {
+        
+        $stars = array();
+        
+        return $this->render(
+            'EntertainmentGuideToTheStarsBundle:Star:index.html.twig',
+            array('stars' => $stars)
+        );
+        
+    }
+    
+    
+    /**
+     * @Route("/stars/create")
+     */
+    public function createAction()
+    {
+       
+        return $this->render(
+            'EntertainmentGuideToTheStarsBundle:Star:create.html.twig'
+        );
+       
+       
+        
+    }
+    
+    
+     /**
+     * @Route("/stars/updateAction")
+     */
+    public function updateAction($starId)
+    {
+        
+    }
+    
+    
+}
