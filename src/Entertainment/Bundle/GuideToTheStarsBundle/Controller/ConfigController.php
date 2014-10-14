0bb8b26889cb3d15a14089b8f4cd2dabfb79db21
Walter Schweitzer <wschweitzer00@gmail.com>
2014-10-14 14:56:15 -0400

    added gts category entity

---
+++ src/Entertainment/Bundle/GuideToTheStarsBundle/Controller/ConfigController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Entertainment\Bundle\GuideToTheStarsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use Entertainment\Bundle\GuideToTheStarsBundle\Entity\GTSconfig;
+
+
+class ConfigController extends Controller
+{
+    
+    /**
+    * @Route("/stars/config/{eventId}")
+    * @Template()
+    */
+    public function indexAction($eventId)
+    {
+         $event = $this->getDoctrine()
+            ->getRepository('EntertainmentRedCarpetBundle:Event')
+            ->find($eventId);
+         
+        $configRepo = $this->getDoctrine()
+            ->getRepository('EntertainmentGuideToTheStarsBundle:GTSconfig');
+        
+        $configData = $configRepo->findBy(
+                       array('eventId' => $eventId));
+        
+        if(isset($configData)) {
+            
+        }
+        
+        return $this->render(
+            'EntertainmentGuideToTheStarsBundle:Config:index.html.twig'
+        );
+    }
+}       
+       
---
+++ src/Entertainment/Bundle/GuideToTheStarsBundle/Entity/GTSconfig.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Entertainment\Bundle\GuideToTheStarsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * GTSconfig
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class GTSconfig
+{
+    
+   
+    // One to One unidrectional.  Event does not reference the GTS config.  
+    /**
+    * @ORM\OneToOne(targetEntity="Entertainment\Bundle\RedCarpetBundle\Entity\Event")
+    * @ORM\JoinColumn(name="event_id", referencedColumnName="event_id")
+    */
+    
+    private $event;
+    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="gts_id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $gtsId;
+    
+      /**
+     * @var string
+     *
+     * @ORM\Column(name="star_desc", type="string", length=255, nullable=true)
+     */
+    private $starDesc;
+    
+      /**
+     * @var string
+     *
+     * @ORM\Column(name="category_desc", type="string", length=255, nullable=true)
+     */
+    private $categoryDesc;
+  
+
+    /**
+     * Get gtsId
+     *
+     * @return integer 
+     */
+    public function getGtsId()
+    {
+        return $this->gtsId;
+    }
+
+    /**
+     * Set starDesc
+     *
+     * @param string $starDesc
+     * @return GTSconfig
+     */
+    public function setStarDesc($starDesc)
+    {
+        $this->starDesc = $starDesc;
+
+        return $this;
+    }
+
+    /**
+     * Get starDesc
+     *
+     * @return string 
+     */
+    public function getStarDesc()
+    {
+        return $this->starDesc;
+    }
+
+    /**
+     * Set categoryDesc
+     *
+     * @param string $categoryDesc
+     * @return GTSconfig
+     */
+    public function setCategoryDesc($categoryDesc)
+    {
+        $this->categoryDesc = $categoryDesc;
+
+        return $this;
+    }
+
+    /**
+     * Get categoryDesc
+     *
+     * @return string 
+     */
+    public function getCategoryDesc()
+    {
+        return $this->categoryDesc;
+    }
+
+    /**
+     * Set event
+     *
+     * @param \Entertainment\Bundle\RedCarpetBundle\Entity\Event $event
+     * @return GTSconfig
+     */
+    public function setEvent(\Entertainment\Bundle\RedCarpetBundle\Entity\Event $event = null)
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+
+    /**
+     * Get event
+     *
+     * @return \Entertainment\Bundle\RedCarpetBundle\Entity\Event 
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+}
