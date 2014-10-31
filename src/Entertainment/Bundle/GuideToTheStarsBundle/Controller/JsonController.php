4dfb63f0979b9b9113fe83ea85c8c80ed75d59e2
Walter Schweitzer <wschweitzer00@gmail.com>
2014-10-31 14:55:21 -0400

    inital JSON outline

--- src/Entertainment/Bundle/GuideToTheStarsBundle/Controller/JsonController.php
+++ src/Entertainment/Bundle/GuideToTheStarsBundle/Controller/JsonController.php
@@ -3,8 +3,20 @@
 namespace Entertainment\Bundle\GuideToTheStarsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use Entertainment\Bundle\GuideToTheStarsBundle\Entity\GTSstar;
+use Entertainment\Bundle\GuideToTheStarsBundle\Entity\GTSvideo;
+use Entertainment\Bundle\GuideToTheStarsBundle\Entity\GTSimage;
+use Entertainment\Bundle\GuideToTheStarsBundle\Entity\GTSquote;
+use Entertainment\Bundle\GuideToTheStarsBundle\Entity\GTSfunfact;
+
 
 class JsonController extends Controller
 {
@@ -14,7 +26,7 @@ class JsonController extends Controller
      * @Template()
     */
 
-    public function contentJSONAction () {
+    public function desktopJsonAction () {
         $name = array('foo' => 'bar');
         $response = new Response(json_encode(array('name' => $name)));
         $response->headers->set('Content-Type', 'application/json');
@@ -24,11 +36,12 @@ class JsonController extends Controller
     
     
     /**
-     * @Route("/stars/json/mobile")
+     * @Route("/stars/json/mobile/categories")
      * @Template()
     */
 
-    public function contentJSONAction () {
+    public function mobileJsonAction () {
+        ## all categories names w/star name and small image FIRST Category
         $name = array('foo' => 'bar');
         $response = new Response(json_encode(array('name' => $name)));
         $response->headers->set('Content-Type', 'application/json');
@@ -42,7 +55,8 @@ class JsonController extends Controller
      * @Template()
     */
 
-    public function contentJSONAction () {
+    public function starJsonAction () {
+         ## category feed w/ star name and small image
         $name = array('foo' => 'bar');
         $response = new Response(json_encode(array('name' => $name)));
         $response->headers->set('Content-Type', 'application/json');
@@ -50,5 +64,18 @@ class JsonController extends Controller
         return $response;
     }
     
+      /**
+     * @Route("/stars/json/")
+     * @Template()
+    */
+
+    public function starJsonAction () {
+        ## all star info w/content
+        $name = array('foo' => 'bar');
+        $response = new Response(json_encode(array('name' => $name)));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
     
 }
