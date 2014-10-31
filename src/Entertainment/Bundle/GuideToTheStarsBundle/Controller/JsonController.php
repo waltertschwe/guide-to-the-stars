c7f678aa93f615aac98f51ffe76500dcc19d5df6
Walter Schweitzer <wschweitzer00@gmail.com>
2014-10-31 13:53:29 -0400

    modification to new gts stars controller

--- src/Entertainment/Bundle/GuideToTheStarsBundle/Controller/JsonController.php
+++ src/Entertainment/Bundle/GuideToTheStarsBundle/Controller/JsonController.php
@@ -8,4 +8,47 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class JsonController extends Controller
 {
+    
+    /**
+     * @Route("/stars/json/desktop")
+     * @Template()
+    */
+
+    public function contentJSONAction () {
+        $name = array('foo' => 'bar');
+        $response = new Response(json_encode(array('name' => $name)));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+    
+    
+    /**
+     * @Route("/stars/json/mobile")
+     * @Template()
+    */
+
+    public function contentJSONAction () {
+        $name = array('foo' => 'bar');
+        $response = new Response(json_encode(array('name' => $name)));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+    
+    
+    /**
+     * @Route("/stars/json/")
+     * @Template()
+    */
+
+    public function contentJSONAction () {
+        $name = array('foo' => 'bar');
+        $response = new Response(json_encode(array('name' => $name)));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+    
+    
 }
